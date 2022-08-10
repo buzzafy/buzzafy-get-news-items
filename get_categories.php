43670c1eae439f31ef5ff95893f79db9ad188002
MelleVice <mellegeert@gmail.com>
2022-08-10 11:17:08 +0200

    Update get_categories.php
    updated

--- get_categories.php
+++ get_categories.php
@@ -1,10 +1,8 @@
 <?php
 /*
 © 1999 - 2022 Amaxwire.com
-
 This is just an example of how you can query our categories. You will need to store these in a database yourself.
 This information is fixed, and will not be changed -often-.
-
 please contact us if you have any question
 */
 
@@ -20,30 +18,52 @@ $url = file_get_contents('https://amaxwire.com/api/get/?p='.AMAX_KEY_PUBLIC.'&re
 
 
 $content = json_decode($url);
+ 
 
 if($content->status == 200){
 
             
-
-            
-
-    foreach ($content->items as &$item) {
-
  
+    foreach ($content->items as $catid => $item) {
+    
+       
+ 
+      // the category id, news items will have this id, you can use  this, or save it to your database
+      $maincatID = $catid; 
+      
+      // the main category name, you can use  this, or save it to your database 
       $maincatname = $item->catname;
-      /* ^^^^^ this is the main Category name,
-                you can use  this, or save it to your database */
+
+                        /* EG, 
+                        insert into DB table categories
+                        id = $maincatID
+                        belongsto = 0
+                        name = $maincatname                        
+                        */      
+ 
                  
                 
                 /* are there any children of the main category ? */
-                if(!empty($item->subcat)){
+                if(!empty($item->subcats)){
 
-                    foreach ($item->subcat as &$subcatname) {
+                    
 
+                    foreach ($item->subcats as $subid => $subcatname) {
 
+                   
+                        // the sub category id, news items will have this sub cat id, you can use  this, or save it to your database 
+                        $subcatID = $subid;
+                        
+                         // the sub category name, you can use  this, or save it to your database 
                         $subcatname = $subcatname;
-                         /* ^^^^^ this is the sub Category name,
-                                     you can use  this, or save it to your database */
+                        
+                        /* EG, 
+                        insert into DB table categories
+                        id = $subcatID
+                        belongsto = $maincatID
+                        name = $subcatname                        
+                        */
+
 
 
                     }
